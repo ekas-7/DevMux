af1c64b99e80f5c740f995e0ad58942af15a24e4
ekas-7 <ekaspreetatwal@gmail.com>
2025-01-24 02:35:44 +0530

    tabs section

--- app/components/landingpage/macbook.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function Macbook() {
-  return (
-    <div className="overflow-hidden dark:bg-[#0B0B0F] bg-white w-full">
-      
-    </div>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { MagicSection } from "./components/landingpage/magic-section"
 import { TestimonialsSection } from "./components/landingpage/testimonials-section"
 import { Footer } from "./components/landingpage/footer"
 import { Navbar } from "./components/landingpage/navbar"
-import { Macbook } from "./components/landingpage/macbook"
+import { Tab } from "./components/landingpage/tab"
 export default function Home() {
   return (
     <main>
       <Navbar />
       <HeroSection />
-      <Macbook />
+      <Tab />
       <FeaturesSection />
       <MagicSection />
       <TestimonialsSection />
